08a105dc84ccd28591b2cea0f207fa2f50d657bc
ahmedshabaanhelwa <ahmedshabaan@outlook.com>
2021-03-11 01:45:26 +0200

    DbContext and Repository implemented

---
+++ src/Catalog.Infrastructure/CatalogContext.cs
@@ -0,0 +1,45 @@
+﻿using Catalog.Domain;
+using Catalog.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Catalog.Infrastructure
+{
+    public class CatalogContext : DbContext, IUnitOfWork
+    {
+        // Constants
+        public const string DEFAULT_SCHEMA = "catalog";
+        
+        // Properties
+        /// <summary>
+        /// Items table in database
+        /// </summary>
+        public DbSet<Item> Items { set; get; }
+        /// <summary>
+        /// Constructor of contect class to pass options to the DbContext base class
+        /// </summary>
+        /// <param name="options">Options of database to be passed to the ORM</param>
+        public CatalogContext(DbContextOptions<CatalogContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        { 
+        }
+        /// <summary>
+        /// Effective save of changes in database
+        /// </summary>
+        /// <param name="cancellationToken">Allows to stop pending async operations</param>
+        /// <returns>true or false</returns>
+        public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken)
+        {
+            // Calls SaveChangesAsync derived by DbContext class and declared by IUnitOfWork interface.
+            await SaveChangesAsync(cancellationToken);
+            return true;
+        }
+    }
+}
---
+++ src/Catalog.Infrastructure/Repositories/ItemRepository.cs
@@ -0,0 +1,74 @@
+﻿using Catalog.Domain;
+using Catalog.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Infrastructure.Repositories
+{
+    public class ItemRepository : IItemRepository
+    {
+        // Private fields
+        private readonly CatalogContext _context;
+        // Public properties
+        public IUnitOfWork UnitOfWork => _context;
+        public ItemRepository(CatalogContext context)
+        {
+            _context = context ?? throw new 
+                ArgumentNullException(nameof(_context));
+        }
+
+        // Public methods
+        /// <summary>
+        /// Gets a list of items from items table in database
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IEnumerable<Item>> GetAsync()
+        {
+            return await _context.Items
+                .AsNoTracking()
+                .ToListAsync();
+        }
+        /// <summary>
+        /// Gets an item by id
+        /// </summary>
+        /// <param name="id">Id of certain item to be getten</param>
+        /// <returns></returns>
+        public async Task<Item> GetAsync(Guid id)
+        {
+            var item = await _context.Items
+                .AsNoTracking()
+                .Where(x => x.Id == id)
+                .Include(x => x.Genre)
+                .Include(x => x.Artist)
+                .FirstOrDefaultAsync();
+            return item;
+        }
+        /// <summary>
+        /// Adds an item to items table in database
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public Item Add(Item item)
+        {
+            return _context.Items.Add(item).Entity;
+        }
+        /// <summary>
+        /// Updates an existing item in items table in database
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public Item Update(Item item)
+        {
+            /* returns the entry of the passed item entity 
+             * and change the state to modified sothat being effectively
+             * saved by UnitOfWork
+             */
+            _context.Entry(item).State = EntityState.Modified;
+            return item;
+        }
+    }
+}
