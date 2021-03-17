04e78126e36ccea97bd70cc661c6271849239c27
ahmedshabaanhelwa <ahmedshabaan@outlook.com>
2021-03-18 00:40:38 +0200

    Registering the service and pre-migration configurations

---
+++ src/Catalog.API/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Catalog.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Extensions
+{
+    public static class DatabaseExtensions
+    {
+        public static IServiceCollection AddCatalogContext(this IServiceCollection services, string connectionString)
+        {
+             services.AddEntityFrameworkSqlServer();
+            services.AddDbContext<CatalogContext>(
+                contextOptions => contextOptions.UseSqlServer(connectionString,
+                                serverOptions =>
+                                {
+                                    serverOptions.MigrationsAssembly(
+                                        typeof(Startup)
+                                        .GetTypeInfo()
+                                        .Assembly
+                                        .GetName()
+                                        .Name);
+                                } // end of serverOptions
+                        ) // End of UseSqlServer
+                );
+
+            return services;
+        }
+    }
+}
--- src/Catalog.API/Startup.cs
+++ src/Catalog.API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Catalog.API.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,8 @@ namespace Catalog.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            // Call the extension method of catalog service
+            services.AddCatalogContext(Configuration.GetSection("DataSource:ConnectionString").Value);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
