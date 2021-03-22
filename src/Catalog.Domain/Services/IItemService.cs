8748801c9a14320ffe96841be300e94bce495149
ahmedshabaanhelwa <ahmedshabaan@outlook.com>
2021-03-22 16:18:49 +0200

    Item service interface | Ch. 9

---
+++ src/Catalog.Domain/Services/IItemService.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.Domain.Requests.Item;
+using Catalog.Domain.Responses.Item;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Domain.Services
+{
+    /// <summary>
+    /// Item service interface
+    /// </summary>
+    public interface IItemService
+    {
+        Task<IEnumerable<ItemResponse>> GetItemsAsync();
+        Task<ItemResponse> GetItemAsync(GetItemRequest request);
+        Task<ItemResponse> AddItemAsync(AddItemRequest request);
+        Task<ItemResponse> EditItemAsync(EditItemRequest request);
+        // Delete opertion to be implemented as soft delete later
+    }
+}
